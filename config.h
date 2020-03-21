5d07e8320222c4f3dc3e4a4ee65c7136cfadbf53
Kipparis <keepintine@gmail.com>
2020-03-21 14:35:35 +0300

    setted up initial layout

--- config.h
+++ config.h
@@ -194,4 +194,6 @@ static Button buttons[] = {
 	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 0 },       1 },
 };
 
-#define HOMEPAGE "https://duckduckgo.com/"
+// #define HOMEPAGE "https://duckduckgo.com/"
+#define HOMEPAGE "file:///home/kip/.surf/homepage.html"
+
